9c197bf4e8af0931cad6adf567dfdf203e24c35c
Andrew Petersen <realandrewpetersen@gmail.com>
2026-01-29 18:50:20 -0500

    Seal movement

--- seal.gd
+++ seal.gd
@@ -1,42 +1,55 @@
 extends Node2D
 
 
+
 var points = [
-	Vector2(Vector2.ZERO)
 ]
+var radii = [
+	
+]
+@onready var segments = [
+	$Head,
+	$Neck,
+	$Body,
+	$Tail1,
+	$Tail2,
+	$Tail3,
+]
+
+func _ready() -> void:
+	for i in range(segments.size()-1):
+		var seg: Node2D = segments[i]
+		var next: Node2D = segments[i+1]
+		points.append(seg.position)
+		var diff = next.position - seg.position
+		radii.append(diff.length())
+		
+	points.append(segments[segments.size()-1].position)
 
 func enforce_distance(reverse=0):
 	var inc = 1
 	if reverse:
 		inc = -1
-		
-	var max_dist = 5
-	var foo = Vector2.ZERO
-	foo.normalized()
 	
-	# if not reversed: points[1] gets restricted by points[0]
 	var start = points.size() * reverse
 	var stop = (points.size() * (1 - reverse)) - inc
-	print(points.size())
-	print(start)
-	
-	print(stop)
 	for i in range(start, stop):
 		var pt = points[i]
 		var pt2 = points[i+inc]
 		var diff = pt2 - pt
-		diff = diff.normalized() * max_dist
+		diff = diff.normalized() * radii[i]
 		var new_pt2 = pt + diff
 		points[i+inc] = new_pt2
-
-func _init() -> void:
-	var pt = points[0]
-	for i in range(5):
-		pt = Vector2(pt) + Vector2(-5, 0)
-		points.append(pt)
+	
+	for i in range(points.size()):
+		segments[i].position = points[i]
 		
+func do_rotation():
+	for i in range(segments.size()-1, 0, -1):
+		segments[i].look_at(segments[i-1].position)
+	
 func _process(delta: float) -> void:
-	var speed = 20
+	var speed = 200
 	if Input.is_key_pressed(KEY_A):
 		points[0].x -= speed * delta
 	if Input.is_key_pressed(KEY_D):
@@ -48,6 +61,7 @@ func _process(delta: float) -> void:
 		points[0].y += speed * delta
 		
 	enforce_distance()
+	do_rotation()
 	
 	queue_redraw()
 		
